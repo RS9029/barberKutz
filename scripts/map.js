e4b245f74ca1a7e421a82d1def0c040d14fff33b
Rafael Silva <rafaelsilva@northnerds.com>
2021-02-28 16:17:33 -0500

    added integrated booking system

--- scripts/map.js
+++ scripts/map.js
@@ -1,5 +1,10 @@
+
+// function to init google maps integration 
+
 function initMap(){
     let myMap = document.getElementById("map");
+
+    // configs to personalize google map
     let options = {
       zoom:14,
       center:{
@@ -10,6 +15,7 @@ function initMap(){
 
     let map = new google.maps.Map(myMap,options);
 
+    // controls marker on the map
     let marker = new google.maps.Marker({
       position:{lat:42.984924,lng:-81.245277},
       map:map,
--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,15 @@
 let links = document.querySelectorAll(".links");
 let mustache = document.createElement('img');
 const nav = document.getElementById('top-nav');
+
+
+// Initial nav mustache config
 mustache.setAttribute('src',"/resources/mustache.svg");
 mustache.classList.add('mustache-underline');
 document.body.append(mustache);
 
 
-
+// functions that controls how the mustache in the nav bar moves from link to link
 function mustacheMove(){
     let linkPosition = this.getBoundingClientRect();
     
@@ -22,9 +25,14 @@ function mustacheMove(){
     mustache.style.transform = `translate(${coordinates.leftOffset}px, ${coordinates.topOffset}px)`;
 }
 
+
+// functions that controls when user isnt hovering over a link for the mustache to disappear 
 function mustacheRemove(){
     mustache.style.display = "none";
 }
+
+
+// mustache event listeners
 
 links.forEach(link=>{
     link.addEventListener('mouseenter',mustacheMove);
